2272fe984288826d72ad949ce65a7e542e989114
armando <armandoleonet95@gmail.com>
2021-12-17 10:47:11 -0400

    delete the unnecesary huouse image folder and adding the git ignore file

---
+++ Tesis/Controllers/TrustController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Tesis.Extensions;
+using Tesis.Interfaces;
+
+namespace Tesis.Controllers
+{
+    [Authorize]
+    public class TrustController : BaseApiController
+    {
+        private readonly IUserRepository _userRepo;
+        private readonly ITrustRepository _trustRepo;
+
+        public TrustController(IUserRepository userRepo, ITrustRepository trustRepo)
+        {
+            _userRepo = userRepo;
+            _trustRepo = trustRepo;
+        }
+
+        //[HttpPost("{username}")]
+        //public async Task<ActionResult> AddLike(string username)
+        //{
+        //    var sourceUserId = User
+
+        //    var likedUser = await _userRepo.GetUserByUsernameAsync(username);
+        //    var sourceUsrId = await _userRepo.getuser
+        //    //var sourceUser = await _userRepo.getuse
+
+        //    return Ok();
+        //}
+    }
+}
